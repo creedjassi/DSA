9c96efb185d1109ff0e8efa1a12803fe9469bea4
Jasroop Singh <jasroop9@gmail.com>
2022-08-02 11:39:46 +0530

    init

---
+++ leftmost_index_of_x_sorted_desc_array.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int leftIndex(int arr[],int n,int x)
+{   
+    int l=0,h=n-1;
+    while(l<=h)
+    {
+        int mid=(l+h)/2;
+        if(arr[mid]==x && (arr[mid]!=arr[mid-1]||mid==0))
+           return mid;
+        else if(arr[mid]>x)
+                l=mid+1;
+             else if(arr[mid]<x)
+                    h=mid-1;
+                  else h=mid-1;
+    }
+    return -1;
+}
+int main()
+{   int a[]={20,20,20,20,20,20,10},n=7,x=20;
+    cout<<leftIndex(a,n,x);
+    return 0;
+}
---
+++ rightmost_index_of_x_sorted_array.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int rightIndex(int arr[],int n, int x)
+{  int l=0,h=n-1;
+   while(l<=h)
+   {
+    int mid=(l+h)/2;
+    if(arr[mid]==x && (arr[mid]!=arr[mid+1]||mid==n-1))
+        return mid;
+    else if(arr[mid]>x)
+            h=mid-1;
+        else if(arr[mid]<x)
+                l=mid+1;
+            else l=mid+1;
+   }
+    return -1;
+}
+int main()
+{   int a[]={10,20,20,20,20,20,20},n=7,x=20;
+    cout<<rightIndex(a,n,x);
+    return 0;
+}
