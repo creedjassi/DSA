03b79e35b8ee7aa4f7f3607c9ab77a3c8b112c88
Jasroop Singh <jasroop9@gmail.com>
2022-08-09 13:03:28 +0530

    init

--- maxIndex.cpp
+++ maxIndex.cpp
@@ -5,8 +5,10 @@ int maxIndexDiff(int arr[], int n)
         int lmin[n]={0};
         int rmax[n]={0};
         lmin[0]=arr[0];
+
         for(int i=1;i<n;i++)
             lmin[i]=min(lmin[i-1],arr[i]);
+            
         rmax[n-1]=arr[n-1];
         for(int j=n-2;j>=0;j--)
            rmax[j]=max(rmax[j+1],arr[j]);
